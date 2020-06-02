3f0c4bcedea24a23d0fcdd28856bcc1986741cf3
dcoleslaw86 <cole.desrosiers86@gmail.com>
2020-06-02 00:56:25 -0500

    formated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,39 +10,39 @@ var computerPicks = choices[randomizedLetter];
 console.log(computerPicks + " picked by the computer.");
 
 function reset() {
-	guessesLeft = 7;
+    guessesLeft = 7;
     document.getElementById("guessesLeft").innerText = "Guesses Left: " + guessesLeft;
     chosenLetters = [];
     document.getElementById("userGuesses").innerText = "Your Guesses so far: ";
     randomizedLetter = Math.floor(Math.random() * choices.length);
     computerPicks = choices[randomizedLetter];
-	console.log(computerPicks + " picked by the computer.");
+    console.log(computerPicks + " picked by the computer.");
 }
-document.onkeyup = function(event) {
-	var userInput = event.key;
+document.onkeyup = function (event) {
+    var userInput = event.key;
     var validChoices = /[a-z]/;
     if (!validChoices.test(userInput)) {
-    alert("please enter a valid letter that is not a symbol.");
+        alert("please enter a valid letter that is not a symbol.");
     }
     else {
-    console.log(userInput);
+        console.log(userInput);
     }
-	if(userInput !== computerPicks){
+    if (userInput !== computerPicks) {
         guessesLeft--;
         document.getElementById("guessesLeft").innerText = "Guesses Left: " + guessesLeft;
         chosenLetters.push(userInput);
-		document.getElementById("userGuesses").innerText = "Your Guesses so far: " + chosenLetters;
-	}
-	else if (userInput === computerPicks){
+        document.getElementById("userGuesses").innerText = "Your Guesses so far: " + chosenLetters;
+    }
+    else if (userInput === computerPicks) {
         wins++;
         alert("You WIN! Excellent! Your skills far surpass mine.");
         document.getElementById("wins").innerText = "Wins: " + wins;
         reset();
-	}
-	if (guessesLeft === 0){
+    }
+    if (guessesLeft === 0) {
         losses++;
         document.getElementById("lose").innerText = "Loses: " + losses;
         alert("Such a shame, you lost. Try harder next time.");
         reset();
-	} 
-  };
+    }
+};
